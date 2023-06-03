92553f54c019cd39d52ab7b0efaf2c1aafae6080
Aviatorvordoen <happycraftmaster@gmail.com>
2023-06-03 12:43:52 +0300

    c0306

--- js/signin.js
+++ js/signin.js
@@ -1,14 +1,8 @@
-// on load page
-// window.onload = ()=>{
-//   if (localStorage.getItem('NW'))
-//   {
-//     AccessTokens.push(localStorage.getItem('NW'))
-//     localStorage.removeItem('NW');
-//     console.log(AccessTokens);
-//   }
-// }
-
-
+// on page load
+window.onload = ()=>
+{
+  Enter_to_system();
+}
 // get elements
 let bodyel = document.querySelector('body');
 let form = document.getElementById('fg');
@@ -16,17 +10,31 @@ let form = document.getElementById('fg');
 // let reset_label = document.querySelector('.rst_text')
 // let reset_token_link = document.querySelector('.reset_token_link').addEventListener('click', Reset_token)
 bodyel.classList.add('body_content_center_for_access')
-const login_btn = document.getElementById('login_btnid').addEventListener('click', ()=>{
+const login_btn = document.getElementById('login_btnid').addEventListener('click', Enter_to_system)
+
+function Enter_to_system(){
   let AccessTokenInput = document.querySelector('.tokenInput').value
-  // console.log(AccessTokens);
-  // let AccessTokenInput = document.querySelector('.TokenReset_input').value;
-  if ((AccessTokenInput == AccessTokens[0]) || (AccessTokenInput == AccessTokens.at(-1)))
-  {
-    console.log('work');
-    window.location.replace('ldza_chart.html');
+
+  const AccessTokens = 
+  [
+  'eer12sd', 'ssiggnr45977', "fdvOxc", "gz7b8nx", "bqgIEcqdls", "NdVjL2g3", "PFT4Gai",
+  "Y2HkcQi", 'X5Yqcx', 'XNIlu', 'D8XVse2q', 'fdv65odOKFOpQaXP9', 'DhNj5n3', 'qr7XWwqs44',
+  '70TjQhL8', 'eneND', 'koIFI1O', 'IcRI5s1E4H'
+  ]
+
+  if (localStorage.getItem('NT')){
+    AccessTokens.push(localStorage.getItem('NT'))
   }
 
-})
+  if ((AccessTokenInput == AccessTokens[0]) || (AccessTokenInput == AccessTokens.at(-1)))
+  {
+    window.location.replace('./ldza_chart.html');
+  }
+  
+
+
+
+}
 
 function Reset_token()
 {
@@ -35,39 +43,30 @@ function Reset_token()
 
 }
 
-const AccessTokens = 
-[
-  'eer12sd', 'ssiggnr45977', "fdvOxc", "gz7b8nx", "bqgIEcqdls", "NdVjL2g3", "PFT4Gai",
-  "Y2HkcQi", 'X5Yqcx', 'XNIlu', 'D8XVse2q', 'fdv65odOKFOpQaXP9', 'DhNj5n3', 'qr7XWwqs44',
-  '70TjQhL8', 'eneND', 'koIFI1O', 'IcRI5s1E4H'
-]
+function check_valueLS()
+{
+  if (localStorage.getItem('NW')){
+    // AccessTokens.push(localStorage.getItem('NW')) // add to array key from LS for login
+    // console.log(AccessTokens);
+    console.log(AccessTokens);
+  }
+}
 
 form.addEventListener('submit', (e)=>{
   e.preventDefault();
   // console.log('working');
   // valid path
-  if (window.location.pathname == './pages/signIN.html')
-  {
-    // console.log('df')
-    let AccessTokenInput = document.querySelector('.TokenReset_input').value
-    // console.log(AccessTokens);
-    // let AccessTokenInput = document.querySelector('.TokenReset_input').value;
-    // if ((AccessTokenInput == AccessTokens[0]) || (AccessTokenInput == AccessTokens.at(-1)))
-    if (AccessTokenInput == "1")
-    {
-      console.log('work');
-      window.location.replace('.ldza_chart.html');
-    }
-  }
+  // if (window.location.pathname == './pages/signIN.html')
+  // {
+  //   // console.log('df')
+  //   // console.log(AccessTokens);
+  //   // let AccessTokenInput = document.querySelector('.TokenReset_input').value;
+  //   if (AccessTokenInput == "1")
+  //   {
+  //     console.log('work');
+  //     window.location.replace('.ldza_chart.html');
+  //   }
+  // }
+  Enter_to_system()
 
-  if (window.location.pathname == './pages/reset_entry_token.html')
-  {
-    let ResetTokenInputField = document.querySelector('.TokenReset_input').value
-    if (ResetTokenInputField == ResetTokenInputField)
-    {
-      localStorage.setItem("NW", ResetTokenInputField)
-    }
-    
-
-  }
 })
